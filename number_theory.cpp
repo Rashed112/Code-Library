7c2019db46f4365a3f909d2e1b8b13c39639e0c1
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-04-23 18:14:10 +0600

    Update number_theory.cpp

--- number_theory.cpp
+++ number_theory.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 #define M 1000000
 bool marked[M]; //by default all values are zero/false
+class NT{
+    public:
+    bool isPrime(int n){
+        if(n<2) return false;
+        if(n==2) return true;
+        if(n%2==0) return false;
+        return marked[n] == false;
+    }
 
-bool isPrime(int n){
-    if(n<2) return false;
-    if(n==2) return true;
-    if(n%2==0) return false;
-    return marked[n] == false;
-}
-
-bool sieve_of_eratosthenes(int n){
-    for(int i=3; i*i <= n; i+=2){
-        if(marked[i] == false){ //i is a prime
-            for(int j=i*i; j<=n; j+=i){
-                marked[j]=true;
+    bool sieve_of_eratosthenes(int n){
+        for(int i=3; i*i <= n; i+=2){
+            if(marked[i] == false){ //i is a prime
+                for(int j=i*i; j<=n; j+=i){
+                    marked[j]=true;
+                }
             }
         }
     }
-}
+};
+
 
 int main()
 {
@@ -27,7 +30,8 @@ int main()
     while(t--){
         int n;
         cin>>n;
-        if(isPrime(n)){
+        NT ob;
+        if(ob.isPrime(n)){
             cout<<n<<" is a prime!!"<<endl;
         }
         else{
